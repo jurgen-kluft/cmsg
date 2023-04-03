15fa1dd82c8e3e399ca2404f516ae122b4c516ff
Jurgen Kluft <jurgen@virtuosgames.com>
2023-04-03 18:13:24 +0800

    *

--- source/main/include/cmsg/c_msg.h
+++ source/main/include/cmsg/c_msg.h
@@ -11,6 +11,19 @@ namespace ncore
 {
     namespace cmsg
     {
+        template <typename T>
+        post(gMsgSystem, const char * system, const char * path, const char * identity, T const& msg);
+
+        /*
+        vector3_t pos(0, 0, 0);
+
+        // what about composing a message using named (registered) properties?
+        explosion_msg_t msg(pos, 10.0f, 150);
+
+        cmsg::post(gMsgSystem, "go", "ships", "enemy", msg);
+        cmsg::post(gMsgSystem, "gfx", "vfx", "explosion", explosion_msg_t(pos, 10.0f, 150));
+        cmsg::post(gMsgSystem, "audio", "sfx", "explosion", explosion_msg_t(pos, 10.0f, 150));
+        */
 
     } // namespace cmsg
 } // namespace ncore
