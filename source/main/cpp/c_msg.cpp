e6e724a67f96b58249a3c370de96fb7546e2380f
Jurgen Kluft <jurgen@virtuosgames.com>
2023-04-04 18:03:11 +0800

    * notes

--- source/main/cpp/c_msg.cpp
+++ source/main/cpp/c_msg.cpp
@@ -6,6 +6,19 @@ namespace ncore
 {
     namespace cmsg
     {
+        // so the idea is to easily send a message to:
+
+        // id = name-id, instance-id, resource-id
+
+        // - a game object by id
+        // - a component by id
+        // - a group by id
+
+        // this includes certain short hand notations:
+        // - . within the scope of this game object
+        // - * within the scope of the parent
+        // - # within the scope of a group
+    
 
     } // namespace cmsg
 } // namespace ncore
