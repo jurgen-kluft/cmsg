56c68564ad33b24a97e3859d56f8948d2ad884c1
jurgen.kluft <jurgen.kluft@gmail.com>
2023-04-06 20:46:54 +0800

    * update

--- source/main/cpp/c_msg.cpp
+++ source/main/cpp/c_msg.cpp
@@ -17,39 +17,69 @@ namespace ncore
     class SystemShips : public ecs_system::ihandler
     {
     public:
+            virtual void on_msg(entity_t entity, component_t component, msg_t msg)
+            {
+
+            }
     };
     SystemShips* gSystemShips = nullptr;
 
     class SystemAI : public ecs_system::ihandler
     {
     public:
+            virtual void on_msg(entity_t entity, component_t component, msg_t msg)
+            {
+                
+            }
     };
     SystemAI* gSystemAI = nullptr;
 
     class SystemGfxVfx : public ecs_system::ihandler
     {
     public:
+            virtual void on_msg(entity_t entity, component_t component, msg_t msg)
+            {
+                
+            }
     };
     SystemGfxVfx* gSystemGfxVfx = nullptr;
 
     class SystemAudioSfx : public ecs_system::ihandler
     {
     public:
+            virtual void on_msg(entity_t entity, component_t component, msg_t msg)
+            {
+                
+            }
     };
     SystemAudioSfx* gSystemAudioSfx = nullptr;
 
     class SystemRender : public ecs_system::ihandler
     {
     public:
+            virtual void on_msg(entity_t entity, component_t component, msg_t msg)
+            {
+                
+            }
     };
     SystemRender* gSystemRender = nullptr;
 
     struct vector3_t
     {
-        vector3_t() : x(0), y(0), z(0) {}
-        vector3_t(float x, float y, float z) : x(x), y(y), z(z) {}
+        vector3_t()
+            : x(0)
+            , y(0)
+            , z(0)
+        {
+        }
+        vector3_t(float x, float y, float z)
+            : x(x)
+            , y(y)
+            , z(z)
+        {
+        }
 
-        float x,y,z;
+        float x, y, z;
 
         static vector3_t zero;
         static vector3_t up;
@@ -61,17 +91,15 @@ namespace ncore
 
         ecs_system ecs(allocator);
 
-        system_t    ships_system   = ecs.register_system(gSystemShips, "system/ships");
-        component_t ship_pos_cp    = ecs.register_component<vector3_t>(ships_system, "position");
-        component_t ship_dir_cp    = ecs.register_component<vector3_t>(ships_system, "direction");
-        component_t ship_vel_cp    = ecs.register_component<float>(ships_system, "speed");
-        component_t ship_radius_cp = ecs.register_component<float>(ships_system, "radius");
+        system_t    ships_system = ecs.register_system(gSystemShips, "system/ships");
+        component_t ship_pos_cp  = ecs.register_component<vector3_t>(ships_system, "position");
+        component_t ship_dir_cp  = ecs.register_component<vector3_t>(ships_system, "direction");
 
         entity_t bug1 = ecs.create_entity(ships_system, "entity/bug1");
-        ecs.add_component<vector3_t>(bug1, ship_pos_cp, vector3_t::zero);
-        ecs.add_component<vector3_t>(bug1, ship_dir_cp, vector3_t::up);
-        ecs.add_component<float>(bug1, ship_vel_cp, 10.0f);
-        ecs.add_component<float>(bug1, ship_radius_cp, 1.0f);
+        ecs.add_component<vector3_t>(bug1, ship_pos_cp, vector3_t::zero); // by id
+        ecs.add_component<vector3_t>(bug1, ship_dir_cp, vector3_t::up);   // by id
+        ecs.add_component<f32>(bug1, "speed", 10.0f);                   // by name
+        ecs.add_component<f32>(bug1, "radius", 1.0f);                   // by name
 
         system_t ai_system     = ecs.register_system(gSystemAI, "system/ai");
         system_t vfx_system    = ecs.register_system(gSystemGfxVfx, "system/gfx/vfx");
@@ -84,7 +112,6 @@ namespace ncore
 
         property_t pos_prop    = msg.register_property<vector3_t>("position", vector3_t::zero);
         property_t radius_prop = msg.register_property<float>("radius", 10.0f);
-        property_t damage_prop = msg.register_property<float>("damage", 1.0f);
 
         // what about composing a message using named (registered) properties?
         vector3_t explosion_pos(100, 2, 5);
@@ -93,16 +120,9 @@ namespace ncore
         msg_t explosion_msg = msg.begin(explosion_msg_id, 3);
         {
             // properties by id
-            msg.write_property<vector3_t>(explosion_msg, pos_prop, explosion_pos);
-            msg.write_property<float>(explosion_msg, radius_prop, 10.0f);
-            msg.write_property<float>(explosion_msg, damage_prop, 0.9f);
-
-            // or
-
-            // properties by name
-            msg.write_property<vector3_t>(explosion_msg, "position", explosion_pos);
-            msg.write_property<float>(explosion_msg, "radius", 10.0f);
-            msg.write_property<float>(explosion_msg, "damage", 0.9f);
+            msg.write_property<vector3_t>(explosion_msg, pos_prop, explosion_pos); // by id
+            msg.write_property<f32>(explosion_msg, radius_prop, 10.0f);          // by id
+            msg.write_property<f32>(explosion_msg, "damage", 0.9f);              // by name
         }
         msg.end(explosion_msg);
 
