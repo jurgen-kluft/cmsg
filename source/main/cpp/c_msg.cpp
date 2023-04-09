ed17e42daece7645ba0d9926a7b12b6e1d14d8f7
jurgen.kluft <jurgen.kluft@gmail.com>
2023-04-09 14:45:43 +0800

    * update

--- source/main/cpp/c_msg.cpp
+++ source/main/cpp/c_msg.cpp
@@ -1,6 +1,7 @@
 #include "cbase/c_allocator.h"
 #include "cbase/c_context.h"
 #include "cbase/c_memory.h"
+#include "cbase/c_hbb.h"
 
 #include "cmsg/c_msg.h"
 
@@ -14,53 +15,38 @@ namespace ncore
     // to avoid memory fragmentation and to allow for fast allocation and no deallocation.
     //
 
-    class SystemShips : public ecs_system::ihandler
+    class SystemShips : public ecs_t::ihandler
     {
     public:
-            virtual void on_msg(entity_t entity, component_t component, msg_t msg)
-            {
-
-            }
+        virtual void on_msg(entity_t entity, component_t component, msg_t msg) {}
     };
     SystemShips* gSystemShips = nullptr;
 
-    class SystemAI : public ecs_system::ihandler
+    class SystemAI : public ecs_t::ihandler
     {
     public:
-            virtual void on_msg(entity_t entity, component_t component, msg_t msg)
-            {
-                
-            }
+        virtual void on_msg(entity_t entity, component_t component, msg_t msg) {}
     };
     SystemAI* gSystemAI = nullptr;
 
-    class SystemGfxVfx : public ecs_system::ihandler
+    class SystemGfxVfx : public ecs_t::ihandler
     {
     public:
-            virtual void on_msg(entity_t entity, component_t component, msg_t msg)
-            {
-                
-            }
+        virtual void on_msg(entity_t entity, component_t component, msg_t msg) {}
     };
     SystemGfxVfx* gSystemGfxVfx = nullptr;
 
-    class SystemAudioSfx : public ecs_system::ihandler
+    class SystemAudioSfx : public ecs_t::ihandler
     {
     public:
-            virtual void on_msg(entity_t entity, component_t component, msg_t msg)
-            {
-                
-            }
+        virtual void on_msg(entity_t entity, component_t component, msg_t msg) {}
     };
     SystemAudioSfx* gSystemAudioSfx = nullptr;
 
-    class SystemRender : public ecs_system::ihandler
+    class SystemRender : public ecs_t::ihandler
     {
     public:
-            virtual void on_msg(entity_t entity, component_t component, msg_t msg)
-            {
-                
-            }
+        virtual void on_msg(entity_t entity, component_t component, msg_t msg) {}
     };
     SystemRender* gSystemRender = nullptr;
 
@@ -89,7 +75,7 @@ namespace ncore
     {
         alloc_t* allocator = context_t::system_alloc();
 
-        ecs_system ecs(allocator);
+        ecs_t ecs(allocator);
 
         system_t    ships_system = ecs.register_system(gSystemShips, "system/ships");
         component_t ship_pos_cp  = ecs.register_component<vector3_t>(ships_system, "position");
@@ -98,8 +84,8 @@ namespace ncore
         entity_t bug1 = ecs.create_entity(ships_system, "entity/bug1");
         ecs.add_component<vector3_t>(bug1, ship_pos_cp, vector3_t::zero); // by id
         ecs.add_component<vector3_t>(bug1, ship_dir_cp, vector3_t::up);   // by id
-        ecs.add_component<f32>(bug1, "speed", 10.0f);                   // by name
-        ecs.add_component<f32>(bug1, "radius", 1.0f);                   // by name
+        ecs.add_component<f32>(bug1, "speed", 10.0f);                     // by name
+        ecs.add_component<f32>(bug1, "radius", 1.0f);                     // by name
 
         system_t ai_system     = ecs.register_system(gSystemAI, "system/ai");
         system_t vfx_system    = ecs.register_system(gSystemGfxVfx, "system/gfx/vfx");
@@ -121,8 +107,8 @@ namespace ncore
         {
             // properties by id
             msg.write_property<vector3_t>(explosion_msg, pos_prop, explosion_pos); // by id
-            msg.write_property<f32>(explosion_msg, radius_prop, 10.0f);          // by id
-            msg.write_property<f32>(explosion_msg, "damage", 0.9f);              // by name
+            msg.write_property<f32>(explosion_msg, radius_prop, 10.0f);            // by id
+            msg.write_property<f32>(explosion_msg, "damage", 0.9f);                // by name
         }
         msg.end(explosion_msg);
 
@@ -136,5 +122,108 @@ namespace ncore
         msg.post(vfx_system, explosion_msg);
         msg.post(sfx_system, explosion_msg);
     }
+
+    // the standard types that can be used as ecs components and message properties
+    const type_t<u8>   ecs_t::type_u8;
+    const type_t<u16>  ecs_t::type_u16;
+    const type_t<u32>  ecs_t::type_u32;
+    const type_t<u64>  ecs_t::type_u64;
+    const type_t<s8>   ecs_t::type_s8;
+    const type_t<s16>  ecs_t::type_s16;
+    const type_t<s32>  ecs_t::type_s32;
+    const type_t<s64>  ecs_t::type_s64;
+    const type_t<f32>  ecs_t::type_f32;
+    const type_t<bool> ecs_t::type_bool;
+
+    // the defaults of the above types are:
+    u8   type_t<u8>::default_value   = 0;
+    u16  type_t<u16>::default_value  = 0;
+    u32  type_t<u32>::default_value  = 0;
+    u64  type_t<u64>::default_value  = 0;
+    s8   type_t<s8>::default_value   = 0;
+    s16  type_t<s16>::default_value  = 0;
+    s32  type_t<s32>::default_value  = 0;
+    s64  type_t<s64>::default_value  = 0;
+    f32  type_t<f32>::default_value  = 0.0f;
+    bool type_t<bool>::default_value = false;
+
+    ecs_t::ecs_t(alloc_t* allocator, u32 max_ids, u32 max_systems, u32 max_components)
+        : msg(allocator)
+    {
+    }
+
+    // the name is hashed and stored in an array of ids that is sorted by hash but also contains an index to the name
+    // the name is stored in a separate array of strings and not available in the release/final build
+    // hash collisions are not allowed
+
+    struct id_data_t
+    {
+        u32 m_hash;
+        u32 m_offset;
+    };
+
+    struct named_item_t
+    {
+        u32 m_index;  // index into the item array
+        u32 m_offset; // offset into the name array
+    };
+
+    struct component_info_t
+    {
+        u32   m_sizeof;
+        void* m_type; // pointer, &type_t<T>::default_value
+    };
+
+    struct system_info_t
+    {
+        u32              m_max_components;
+        ecs_t::ihandler* m_handler;
+    };
+
+    template <typename T> struct array_t
+    {
+        T*  m_data;
+        u32 m_size;
+        u32 m_capacity;
+    };
+
+    struct ecs_system_data_t
+    {
+        ecs_data_t* m_ecs; // pointer to the ecs data
+
+        array_t<u16>   m_component_remap; // set to ecs 'max' components
+        array_t<void*> m_component_data;  // set to ecs-system 'max' components
+
+        hbb_data_t  m_entity_used;             // set to ecs-system 'max' entities
+        hbb_hdr_t   m_entity_cp_used_hbb_hdr;  // hbb hdr for used components
+        array_t<u8> m_entity_cp_used_hbb_data; // entity, hbb data per entity to indicate which component is used
+    };
+
+    struct ecs_data_t
+    {
+        alloc_t* m_allocator;
+
+        // id_t - name
+        array_t<id_data_t> m_ids;   // capacity = max-ids
+        array_t<char>      m_names; // the names are stored in a single array of chars
+
+        // when registering a component by name, the name is hashed and the hash is used to find the component
+        // but then when we insert (sorted) it into the component array, we need to know the index of the component
+        hbb_hdr_t                 m_component_free_hbb_hdr;
+        hbb_data_t                m_component_free_hbb_data;
+        array_t<named_item_t>     m_component_names; // (sorted by hash) capacity = max-components
+        array_t<component_info_t> m_component_array; // capacity = max-components
+
+        // when registering a system by name (or getting by name), the name is hashed and the hash is used to find the system
+        // but then when we insert (sorted) it into the system array, we need to know the index of the component
+        hbb_hdr_t                  m_system_free_hbb_hdr;
+        hbb_data_t                 m_system_free_hbb_data;
+        array_t<named_item_t>      m_system_names; // (sorted by hash) capacity = max-components
+        array_t<system_info_t>     m_system_array; // capacity = max-components
+        array_t<ecs_system_data_t> m_system_array;
+    };
+
+    id_t        ecs_t::register_id(const char* name) {}
+    const char* ecs_t::nameof_id(id_t system) {}
 
 } // namespace ncore
--- source/main/include/cmsg/c_msg.h
+++ source/main/include/cmsg/c_msg.h
@@ -9,10 +9,12 @@
 
 namespace ncore
 {
+    class alloc_t;
+
     typedef u32   id_t;        // 32
-    typedef u32   system_t;    // 9 bits for system
-    typedef u32   entity_t;    // 9 bits for system, 18 bits for entity, 5 bits for salt
-    typedef u32   component_t; // 9 bits for system, 18 bits for component, 5 bits for salt
+    typedef u32   system_t;    // 9 bits for system, 3 bits for type,
+    typedef u32   entity_t;    // 9 bits for system, 3 bits for type, 16 bits for entity, 4 bits for salt
+    typedef u32   component_t; // 9 bits for system, 3 bits for type, 16 bits for component, 4 bits for salt
     typedef u32   msg_t;
     typedef void* property_t;
     typedef void* value_t;
@@ -22,12 +24,13 @@ namespace ncore
         static T default_value;
     };
 
-
     // ------------------------------------------------------------------------------------------------
     // message
     class msg_system
     {
     public:
+        msg_system(alloc_t* alloc);
+
         template <typename T> property_t register_property(id_t id, const T& default_value = type_t<T>::default_value);
         template <typename T> property_t register_property(const char* name, const T& default_value = type_t<T>::default_value);
 
@@ -64,31 +67,22 @@ namespace ncore
         void post(const char* system, const char* entity, const char* component, msg_t msg);
     };
 
-
-    class ecs_system
+    struct ecs_data_t;
+    class ecs_t
     {
     public:
-        const entity_t    kEntityInvalid    = 0;
-        const entity_t    kEntityAll        = 0xffffffffffffffff;
-        const component_t kComponentAll     = 0xffffffff;
-        const char* const kEntityNameAll    = "*";
-        const char* const kComponentNameAll = "*";
-
-        template <typename T> struct type_t
-        {
-            static T default_value;
-        };
-
-        const type_t<u8>   type_u8;
-        const type_t<u16>  type_u16;
-        const type_t<u32>  type_u32;
-        const type_t<u64>  type_u64;
-        const type_t<s8>   type_s8;
-        const type_t<s16>  type_s16;
-        const type_t<s32>  type_s32;
-        const type_t<s64>  type_s64;
-        const type_t<f32>  type_f32;
-        const type_t<bool> type_bool;
+        // ------------------------------------------------------------------------------------------------
+        // types
+        static const type_t<u8>   type_u8;
+        static const type_t<u16>  type_u16;
+        static const type_t<u32>  type_u32;
+        static const type_t<u64>  type_u64;
+        static const type_t<s8>   type_s8;
+        static const type_t<s16>  type_s16;
+        static const type_t<s32>  type_s32;
+        static const type_t<s64>  type_s64;
+        static const type_t<f32>  type_f32;
+        static const type_t<bool> type_bool;
 
         // ------------------------------------------------------------------------------------------------
         class ihandler
@@ -97,7 +91,7 @@ namespace ncore
             virtual void on_msg(entity_t entity, component_t component, msg_t msg) = 0;
         };
 
-        ecs_system(alloc_t* allocator);
+        ecs_t(alloc_t* allocator, u32 max_ids = 65536, u32 max_systems = 256, u32 max_components = 4096);
 
         // ------------------------------------------------------------------------------------------------
         // id
@@ -130,8 +124,9 @@ namespace ncore
 
         // system - component
 
-        template <typename T> component_t register_component(system_t system, id_t id, const T* default_value = &default_component_t<T>::default_value);
-        template <typename T> component_t register_component(system_t system, const char* name, const T* default_value = &default_component_t<T>::default_value);
+        template <typename T> component_t register_component(system_t system, id_t id, const T* default_value = &type_t<T>::default_value);
+        template <typename T> component_t register_component(system_t system, const char* name, const T* default_value = &type_t<T>::default_value);
+        template <typename T> bool        typeof_component(component_t component, const T* default_value = &type_t<T>::default_value);
         id_t                              idof_component(component_t component);
         const char*                       nameof_component(component_t component);
 
@@ -141,75 +136,14 @@ namespace ncore
 
         template <typename T> void add_component(entity_t entity, component_t component, const T& default_value);
         template <typename T> void add_component(entity_t entity, const char* component, const T& default_value);
-        template <typename T> bool is_typeof(entity_t entity, component_t component, const T* default_value = &default_component_t<T>::default_value);
         template <typename T> bool has_component(entity_t entity, component_t component);
         template <typename T> bool get_component(entity_t entity, component_t component, T& outValue);
         template <typename T> bool get_component(entity_t entity, component_t component, T*& outValue);
         void                       remove_component(entity_t entity, component_t component);
 
-        msg_system msg;
+        msg_system  msg;
+        ecs_data_t* data;
     };
-
-    /*
-
-    Note: Using strings for system, entity and component names is not recommended for performance reasons.
-    ecs_system ecs(allocator);
-
-    system_t ships_system      = ecs.register_system(gSystemShips, "system/ships");
-    component_t ship_pos_cp    = ecs.register_component<vector3_t>(ships_system, "position", sizeof(vector3_t));
-    component_t ship_dir_cp    = ecs.register_component<vector3_t>(ships_system, "direction", sizeof(vector3_t));
-    component_t ship_vel_cp    = ecs.register_component<float>(ships_system, "speed", sizeof(float));
-    component_t ship_radius_cp = ecs.register_component<float>(ships_system, "radius", sizeof(float));
-
-    entity_t bug1 = ecs.create_entity(ships_system, "entity/bug1");
-    ecs.add_component<vector3_t>(bug1, ship_pos_cp);
-    ecs.add_component<vector3_t>(bug1, ship_dir_cp, vector3_t::up);
-    ecs.add_component<float>(bug1, ship_vel_cp, 10.0f);
-    ecs.add_component<float>(bug1, ship_radius_cp, 1.0f);
-
-    system_t ai_system     = ecs.register_system(gSystemAI, "system/ai");
-    system_t vfx_system    = ecs.register_system(gSystemGfxVfx, "system/gfx/vfx");
-    system_t sfx_system    = ecs.register_system(gSystemAudioSfx, "system/audio/sfx");
-    system_t render_system = ecs.register_system(gSystemRender, "system/gfx/render");
-
-    msg_system msg = ecs.msg();
-
-    id_t explosion_msg_id = msg.register_id("msg/explosion");
-
-    property_t pos_prop    = msg.registery_property<vector3_t>("position", vector3_t::zero);
-    property_t radius_prop = msg.registery_property<float>("radius", 10.0f);
-    property_t damage_prop = msg.registery_property<float>("damage", 1.0f);
-
-    // what about composing a message using named (registered) properties?
-    vector3_t explosion_pos(100, 2, 5);
-
-    // position, radius, damage
-    explosion_msg = msg.begin(explosion_msg_id, 3);
-    {
-        // properties by id
-        msg.write_property<vector3_t>(explosion_msg, pos_prop, explosion_pos);
-        msg.write_property<float>(explosion_msg, radius_prop, 10.0f);
-        msg.write_property<float>(explosion_msg, damage_prop, 0.9f);
-
-        // or
-
-        // properties by name
-        msg.write_property<vector3_t>(explosion_msg, "position", explosion_pos);
-        msg.write_property<float>(explosion_msg, "radius", 10.0f);
-        msg.write_property<float>(explosion_msg, "damage", 0.9f);
-    }
-    msg.end(explosion_msg);
-
-    // systems by name
-    msg.post("system/ships", explosion_msg);
-    msg.post("system/ships", "entity/bug1", explosion_msg);
-
-    // systems by id
-    msg.post(ships_system, explosion_msg);
-    msg.post(ai_system, explosion_msg);
-    msg.post(vfx_system, explosion_msg);
-    msg.post(sfx_system, explosion_msg);
-    */
 
 } // namespace ncore
 
