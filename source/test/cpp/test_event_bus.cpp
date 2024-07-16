6f412b57ac09fd4f7a37c606234a737732727bc5
jurgen.kluft <jurgen.kluft@gmail.com>
2024-07-16 10:59:33 +0200

    * cmsg, unittest
    * posting multiple events

--- source/test/cpp/test_event_bus.cpp
+++ source/test/cpp/test_event_bus.cpp
@@ -10,14 +10,14 @@ using namespace ncore;
 
 UNITTEST_SUITE_BEGIN(event_bus)
 {
-    UNITTEST_FIXTURE(basics)
+    UNITTEST_FIXTURE(main)
     {
         UNITTEST_ALLOCATOR;
 
         UNITTEST_FIXTURE_SETUP() {}
         UNITTEST_FIXTURE_TEARDOWN() {}
 
-        UNITTEST_TEST(create_destroy)
+        UNITTEST_TEST(create_destroy_bus)
         {
             nevent::event_bus_t* bus = nevent::create_event_bus(Allocator);
             CHECK_NOT_NULL(bus);
@@ -30,7 +30,7 @@ UNITTEST_SUITE_BEGIN(event_bus)
             float b;
         };
 
-        UNITTEST_TEST(create_destroy_event_channel)
+        UNITTEST_TEST(create_destroy_bus_and_event_channel)
         {
             nevent::event_bus_t* bus = nevent::create_event_bus(Allocator);
             CHECK_NOT_NULL(bus);
@@ -38,13 +38,14 @@ UNITTEST_SUITE_BEGIN(event_bus)
             ncore::Callback1<void, TestEvent const&> delegate;
             nevent::register_event_subscriber(bus, delegate);
 
-            nevent::unregister_event_subscriber(bus, delegate);
+            // ...
 
+            nevent::unregister_event_subscriber(bus, delegate);
             nevent::destroy_event_bus(Allocator, bus);
         }
     }
 
-    UNITTEST_FIXTURE(subscribe_process)
+    UNITTEST_FIXTURE(subscribe_post_process)
     {
         UNITTEST_ALLOCATOR;
 
@@ -64,7 +65,7 @@ UNITTEST_SUITE_BEGIN(event_bus)
             sTestEvent.b += a.b;
         }
 
-        UNITTEST_TEST(subscribe)
+        UNITTEST_TEST(subscribe_single_event)
         {
             nevent::event_bus_t* bus = nevent::create_event_bus(Allocator);
             CHECK_NOT_NULL(bus);
@@ -86,6 +87,37 @@ UNITTEST_SUITE_BEGIN(event_bus)
 
             CHECK_EQUAL(1, sTestEvent.a);
             CHECK_EQUAL(2.0f, sTestEvent.b);
+
+            event.a = 3;
+            event.b = 4.0f;
+            nevent::post_event(bus, event);
+
+            process_events(bus);
+
+            CHECK_EQUAL(1 + 3, sTestEvent.a);
+            CHECK_EQUAL(2.0f + 4.0f, sTestEvent.b);
+
+            nevent::unregister_event_subscriber(bus, delegate);
+            nevent::destroy_event_bus(Allocator, bus);
+        }
+
+        UNITTEST_TEST(subscribe_multiple_events)
+        {
+            nevent::event_bus_t* bus = nevent::create_event_bus(Allocator);
+            CHECK_NOT_NULL(bus);
+
+            ncore::Callback1<void, TestEvent const&> delegate;
+            delegate = ProcessEvent;
+
+            nevent::register_event_subscriber(bus, delegate);
+
+            sTestEvent.a = 0;
+            sTestEvent.b = 0.0f;
+
+            TestEvent event;
+            event.a = 1;
+            event.b = 2.0f;
+            nevent::post_event(bus, event);
 
             event.a = 3;
             event.b = 4.0f;
