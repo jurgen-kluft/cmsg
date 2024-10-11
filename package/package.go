e39282768f04deb7c3c5b0d7518bb54e1a283dec
jurgen.kluft <jurgen.kluft@gmail.com>
2024-10-11 08:18:20 +0800

    * cmsg
    * remove dependency on crtti package

--- package/package.go
+++ package/package.go
@@ -3,7 +3,6 @@ package cmsg
 import (
 	cbase "github.com/jurgen-kluft/cbase/package"
 	denv "github.com/jurgen-kluft/ccode/denv"
-	crtti "github.com/jurgen-kluft/crtti/package"
 	cunittest "github.com/jurgen-kluft/cunittest/package"
 )
 
@@ -12,24 +11,19 @@ func GetPackage() *denv.Package {
 	// Dependencies
 	cunittestpkg := cunittest.GetPackage()
 	cbasepkg := cbase.GetPackage()
-	crttipkg := crtti.GetPackage()
 
 	// The main (cmsg) package
 	mainpkg := denv.NewPackage("cmsg")
 	mainpkg.AddPackage(cunittestpkg)
 	mainpkg.AddPackage(cbasepkg)
-	mainpkg.AddPackage(crttipkg)
 
 	// 'cmsg' library
 	mainlib := denv.SetupDefaultCppLibProject("cmsg", "github.com\\jurgen-kluft\\cmsg")
 	mainlib.Dependencies = append(mainlib.Dependencies, cbasepkg.GetMainLib())
-	mainlib.Dependencies = append(mainlib.Dependencies, crttipkg.GetMainLib())
 
 	// 'cmsg' unittest project
 	maintest := denv.SetupDefaultCppTestProject("cmsg"+"_test", "github.com\\jurgen-kluft\\cmsg")
 	maintest.Dependencies = append(maintest.Dependencies, cunittestpkg.GetMainLib())
-	maintest.Dependencies = append(maintest.Dependencies, cbasepkg.GetMainLib())
-	maintest.Dependencies = append(maintest.Dependencies, crttipkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, mainlib)
 
 	mainpkg.AddMainLib(mainlib)
