f9ac8f49b0f6dbc21f39e33afcaf04cd0db87d67
jurgen.kluft <jurgen.kluft@gmail.com>
2026-08-05 09:04:40 +0800

    * adjust to changes in ccode

--- package/package.go
+++ package/package.go
@@ -3,7 +3,7 @@ package cmsg
 import (
 	cbase "github.com/jurgen-kluft/cbase/package"
 	cunittest "github.com/jurgen-kluft/cunittest/package"
-	denv "github.com/jurgen-kluft/gide/denv"
+	denv "github.com/jurgen-kluft/go-ide/denv"
 )
 
 const (
